c55aa0e6888d449dd4d54640cd6886c050439bf4
Raiman Singh <singh.raiman@gmail.com>
2023-03-27 00:27:56 +0530

    new dashboard

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import './App.css';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+
+const data = [
+  { name: 'Jan', uv: 4000, pv: 2400, amt: 2400 },
+  { name: 'Feb', uv: 3000, pv: 1398, amt: 2210 },
+  { name: 'Mar', uv: 2000, pv: 9800, amt: 2290 },
+  { name: 'Apr', uv: 2780, pv: 3908, amt: 2000 },
+  { name: 'May', uv: 1890, pv: 4800, amt: 2181 },
+  { name: 'Jun', uv: 2390, pv: 3800, amt: 2500 },
+  { name: 'Jul', uv: 3490, pv: 4300, amt: 2100 },
+];
+
+const Dashboard = () => {
+  return (
+    <div className="dashboard">
+      <div className="data-section">
+        <div className="data-card">
+          <h3>Users</h3>
+          <h2>2000</h2>
+        </div>
+        <div className="data-card">
+          <h3>Sales</h3>
+          <h2>$20000</h2>
+        </div>
+        <div className="data-card">
+          <h3>Revenue</h3>
+          <h2>$10000</h2>
+        </div>
+      </div>
+      <div className="chart-section">
+        <LineChart width={600} height={300} data={data} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+          <XAxis dataKey="name" />
+          <YAxis />
+          <CartesianGrid strokeDasharray="3 3" />
+          <Tooltip />
+          <Legend />
+          <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
+          <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+        </LineChart>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
